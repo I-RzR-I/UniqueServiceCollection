888c78afbd6b6a2e496ea1a7c149f44f27c1b404
RzR <108324929+I-RzR-I@users.noreply.github.com>
2025-06-17 23:22:14 +0300

    Add internal Enumerable/Collection extensions.

---
+++ src/UniqueServiceCollection/Extensions/InternalCollectionExtensions.cs
@@ -0,0 +1,59 @@
+﻿// ***********************************************************************
+//  Assembly         : RzR.Shared.Services.UniqueServiceCollection
+//  Author           : RzR
+//  Created On       : 2025-06-17 22:42
+// 
+//  Last Modified By : RzR
+//  Last Modified On : 2025-06-17 22:48
+// ***********************************************************************
+//  <copyright file="InternalCollectionExtensions.cs" company="RzR SOFT & TECH">
+//   Copyright © RzR. All rights reserved.
+//  </copyright>
+// 
+//  <summary>
+//  </summary>
+// ***********************************************************************
+
+#region U S A G E S
+
+using System.Collections.Generic;
+using System.Linq;
+
+#endregion
+
+namespace UniqueServiceCollection.Extensions
+{
+    /// -------------------------------------------------------------------------------------------------
+    /// <summary>
+    ///     A data collection extensions.
+    /// </summary>
+    /// =================================================================================================
+    internal static class InternalCollectionExtensions
+    {
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        ///     An IEnumerable&lt;T&gt; extension method that query if 'collection' has any.
+        /// </summary>
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        /// <param name="collection">The collection to act on.</param>
+        /// <returns>
+        ///     True if any, false if not.
+        /// </returns>
+        /// =================================================================================================
+        internal static bool HasAnyInCollection<T>(this IEnumerable<T> collection)
+            => collection.IsNotNull() && collection.Any();
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        ///     An IEnumerable&lt;T&gt; extension method that query if 'collection' has no any.
+        /// </summary>
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        /// <param name="collection">The collection to act on.</param>
+        /// <returns>
+        ///     True if no any, false if not.
+        /// </returns>
+        /// =================================================================================================
+        internal static bool HasNoAnyInCollection<T>(this IEnumerable<T> collection)
+            => !collection.HasAnyInCollection();
+    }
+}
---
+++ src/UniqueServiceCollection/Extensions/InternalObjectExtensions.cs
@@ -0,0 +1,49 @@
+﻿// ***********************************************************************
+//  Assembly         : RzR.Shared.Services.UniqueServiceCollection
+//  Author           : RzR
+//  Created On       : 2023-05-12 19:24
+// 
+//  Last Modified By : RzR
+//  Last Modified On : 2023-05-12 19:24
+// ***********************************************************************
+//  <copyright file="InternalObjectExtensions.cs" company="">
+//   Copyright (c) RzR. All rights reserved.
+//  </copyright>
+// 
+//  <summary>
+//  </summary>
+// ***********************************************************************
+
+#region U S A G E S
+
+using CodeSource;
+
+#endregion
+
+namespace UniqueServiceCollection.Extensions
+{
+    /// <summary>
+    ///     Object extensions
+    /// </summary>
+    /// <remarks></remarks>
+    internal static class InternalObjectExtensions
+    {
+        /// <summary>
+        ///     Is null
+        /// </summary>
+        /// <param name="obj">Source data</param>
+        /// <returns></returns>
+        [CodeSource("https://github.com/I-RzR-I/DomainCommonExtensions", "RzR",
+            "DomainCommonExtensions.CommonExtensions.NullExtensions.IsNull", 1)]
+        internal static bool IsNull(this object obj) => obj == null;
+
+        /// <summary>
+        ///     Is null
+        /// </summary>
+        /// <param name="obj">Source data</param>
+        /// <returns></returns>
+        [CodeSource("https://github.com/I-RzR-I/DomainCommonExtensions", "RzR",
+            "DomainCommonExtensions.CommonExtensions.NullExtensions.IsNotNull", 1)]
+        internal static bool IsNotNull(this object obj) => !obj.IsNull();
+    }
+}
